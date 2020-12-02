2d61ebe40ca592a64507023293723fe39380a9ff
Samuel Sarle <samuel@sarle.com>
2020-12-02 14:18:10 +0200

    Initial commit.

---
+++ adv_2020/lib/adv2020.ex
@@ -0,0 +1,18 @@
+defmodule Adv2020 do
+  @moduledoc """
+  Documentation for `Adv2020`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adv2020.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ adv_2020/test/adv2020_test.exs
@@ -0,0 +1,8 @@
+defmodule Adv2020Test do
+  use ExUnit.Case
+  doctest Adv2020
+
+  test "greets the world" do
+    assert Adv2020.hello() == :world
+  end
+end
