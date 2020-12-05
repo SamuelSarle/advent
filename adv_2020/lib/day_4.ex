06d2753a1e1bc223079701ba20ac4d1326abaaff
Samuel Sarle <samuel@sarle.com>
2020-12-05 20:36:57 +0200

    Added answer for part 2 of day 4.

--- adv_2020/lib/day_4.ex
+++ adv_2020/lib/day_4.ex
@@ -20,17 +20,61 @@ defmodule DayFour do
     end)
   end
 
+  # Is there a more idiomatic way of doing this in elixir?
   defp validate_passport(passport) do
-    with {:ok, _} <- Keyword.fetch(passport, :byr),
-         {:ok, _} <- Keyword.fetch(passport, :iyr),
-         {:ok, _} <- Keyword.fetch(passport, :eyr),
-         {:ok, _} <- Keyword.fetch(passport, :hgt),
-         {:ok, _} <- Keyword.fetch(passport, :hcl),
-         {:ok, _} <- Keyword.fetch(passport, :ecl),
-         {:ok, _} <- Keyword.fetch(passport, :pid) do
+    with {:ok, byr} <- Keyword.fetch(passport, :byr),
+         :ok <- validate_byr(String.to_integer(byr)),
+         {:ok, iyr} <- Keyword.fetch(passport, :iyr),
+         :ok <- validate_iyr(String.to_integer(iyr)),
+         {:ok, eyr} <- Keyword.fetch(passport, :eyr),
+         :ok <- validate_eyr(String.to_integer(eyr)),
+         {:ok, hgt} <- Keyword.fetch(passport, :hgt),
+         :ok <- validate_hgt(hgt),
+         {:ok, hcl} <- Keyword.fetch(passport, :hcl),
+         :ok <- validate_hcl(hcl),
+         {:ok, ecl} <- Keyword.fetch(passport, :ecl),
+         :ok <- validate_ecl(ecl),
+         {:ok, pid} <- Keyword.fetch(passport, :pid),
+         :ok <- validate_pid(pid) do
       true
     else
       _ -> false
+    end
+  end
+
+  defp validate_byr(byr) when is_integer(byr) and byr >= 1920 and byr <= 2002, do: :ok
+  defp validate_byr(_), do: :error
+
+  defp validate_iyr(iyr) when is_integer(iyr) and iyr >= 2010 and iyr <= 2020, do: :ok
+  defp validate_iyr(_), do: :error
+
+  defp validate_eyr(eyr) when is_integer(eyr) and eyr >= 2020 and eyr <= 2030, do: :ok
+  defp validate_eyr(_), do: :error
+
+  defp validate_hgt(hgt) do
+    {h, units} = Integer.parse(hgt) # greedy, leaves the rest of the string as the unit
+
+    cond do
+      units == "cm" and h >= 150 and h <= 193 -> :ok
+      units == "in" and h >= 59 and h <= 76 -> :ok
+      true -> :error
+    end
+  end
+
+  defp validate_hcl(hcl) do
+    case String.match?(hcl, ~r/^#[a-fA-F0-9]{6}$/) do
+      true -> :ok
+      _ -> :error
+    end
+  end
+
+  defp validate_ecl(ecl) when ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], do: :ok
+  defp validate_ecl(_), do: :error
+
+  defp validate_pid(pid) do
+    case String.match?(pid, ~r/^[0-9]{9}$/) do
+      true -> :ok
+      _ -> :error
     end
   end
 end
