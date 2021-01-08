ae32d14e0bd36e3c8fe632365ba2c4337929a747
Samuel Sarle <samuel@sarle.com>
2021-01-08 18:08:42 +0200

    Updated answer for day 15 to work for part 2.

--- adv_2020/lib/day_15.ex
+++ adv_2020/lib/day_15.ex
@@ -13,23 +13,41 @@ defmodule DayFifteen do
       |> Enum.map(&String.to_integer/1)
       |> Enum.at(turns - 1)
     else
-      input
-      |> String.split(",", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.reverse()
-      |> Stream.iterate(fn
-        [head | tail] ->
-          new =
-            tail
-            |> Stream.with_index(1)
-            |> Stream.filter(fn {val, _} -> val == head end)
-            |> Stream.map(fn {_, i} -> i end)
-            |> Enum.at(0, 0)
+      table = :ets.new(:history, [:named_table, :ordered_set])
 
-          [new, head | tail]
-      end)
-      |> Enum.at(turns - input_len)
-      |> Enum.at(0)
+      answer =
+        input
+        |> String.split(",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+        |> setup_table_and_params(table)
+        |> Stream.iterate(fn
+          {elem, cur_turn} ->
+            new_elem =
+              case :ets.lookup(table, elem) do
+                [] ->
+                  0
+
+                [{_, last}] ->
+                  abs(cur_turn - last)
+              end
+
+            :ets.insert(table, {elem, cur_turn})
+            {new_elem, cur_turn + 1}
+        end)
+        |> Enum.at(turns - input_len)
+        |> elem(0)
+
+      :ets.delete(table)
+
+      answer
     end
+  end
+
+  defp setup_table_and_params(list, table) do
+    list
+    |> Enum.with_index(1)
+    |> Enum.each(fn x -> :ets.insert(table, x) end)
+
+    {Enum.reverse(list) |> Enum.at(0), Enum.count(list)}
   end
 end
