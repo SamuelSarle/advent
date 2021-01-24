06566565a51bbffba62997c97b97bf1d05ccb66b
Samuel Sarle <samuel@sarle.com>
2021-01-24 20:55:29 +0200

    Added answer for part 2 of day 18.

--- adv_2020/lib/day_18.ex
+++ adv_2020/lib/day_18.ex
@@ -44,3 +44,56 @@ defmodule DayEighteen do
   def eval(a, "+", b), do: a + b
   def eval(a, "*", b), do: a * b
 end
+
+defmodule DayEighteen.PartTwo do
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&reduce/1)
+    |> Enum.sum()
+  end
+
+  def reduce(input) do
+    if Regex.match?(~r/^\d*$/, input) do
+      input |> String.to_integer()
+    else
+      input
+      |> String.replace(~r/(?:^|\()([\d\s\+\*]+)(?:$|\))/, &apply_math/1, global: false)
+      |> reduce()
+    end
+  end
+
+  def apply_math(input) do
+    cond do
+      Regex.match?(~r/^\(?\d+\)?$/, input) -> input |> String.replace(~r/[^\d]/, "")
+      Regex.match?(~r/\+/, input) -> do_addition(input) |> apply_math()
+      :else -> do_multiplication(input) |> apply_math()
+    end
+  end
+
+  def do_addition(input), do: do_operator(input, "+")
+  def do_multiplication(input), do: do_operator(input, "*")
+
+  def do_operator(input, operator) do
+    input
+    |> String.replace(
+      ~r/(\d+) ([#{operator}]) (\d+)/,
+      fn x ->
+        [a, op, b] =
+          x
+          |> String.split()
+          |> Enum.map(fn
+            "*" -> "*"
+            "+" -> "+"
+            n -> n |> String.to_integer()
+          end)
+
+        eval(a, op, b) |> to_string()
+      end,
+      global: false
+    )
+  end
+
+  def eval(a, "+", b), do: a + b
+  def eval(a, "*", b), do: a * b
+end
--- adv_2020/test/day_18_test.exs
+++ adv_2020/test/day_18_test.exs
@@ -7,4 +7,11 @@ defmodule DayEighteenTest do
     assert DayEighteen.solve("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
     assert DayEighteen.solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
   end
+
+  test "part two returns correct result" do
+    assert DayEighteen.PartTwo.solve("1 + 2 * 3 + 4 * 5 + 6") == 231
+    assert DayEighteen.PartTwo.solve("2 * 3 + (4 * 5)") == 46
+    assert DayEighteen.PartTwo.solve("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+    assert DayEighteen.PartTwo.solve("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
+  end
 end
