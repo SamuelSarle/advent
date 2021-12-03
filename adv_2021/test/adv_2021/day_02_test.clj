7087dfce68cdef17d4194e36f9cb49f1584a3922
Samuel Sarle <samuel@sarle.com>
2021-12-03 13:47:38 +0200

    Fix indent

--- adv_2021/test/adv_2021/day_02_test.clj
+++ adv_2021/test/adv_2021/day_02_test.clj
@@ -7,6 +7,6 @@
 (def test-input (str/split-lines "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"))
 
 (deftest test-solutions
- (testing "Solutions are correct for example data."
-  (is (= 150 (solve-1 test-input)))
-  (is (= 900 (solve-2 test-input)))))
+  (testing "Solutions are correct for example data."
+    (is (= 150 (solve-1 test-input)))
+    (is (= 900 (solve-2 test-input)))))
