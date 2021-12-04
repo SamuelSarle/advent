3feb12153671ed7fb177513938edd4b5440212a2
Samuel Sarle <samuel@sarle.com>
2021-12-04 15:43:10 +0200

    Add function for getting input without splitting lines

--- adv_2021/src/adv_2021/core.clj
+++ adv_2021/src/adv_2021/core.clj
@@ -1,8 +1,13 @@
 (ns adv-2021.core
   (:require [clojure.string :as str]))
 
-(defn get-input [day]
-  (let [input (str/split-lines (slurp (str "input/day_" (format "%02d" day )".txt")))]
+(defn get-raw-input
+  [day]
+  (slurp (str "input/day_" (format "%02d" day )".txt")))
+
+(defn get-input
+  [day]
+  (let [input (str/split-lines (get-raw-input day))]
     (if (= 1 (count input))
       (first input)
       input)))
