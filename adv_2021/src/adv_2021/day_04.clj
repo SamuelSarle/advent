3ab4bd8a49ed1040d8d20d1f43f200818ba342ad
Samuel Sarle <samuel@sarle.com>
2021-12-04 21:21:43 +0200

    Faster day 4

--- adv_2021/src/adv_2021/day_04.clj
+++ adv_2021/src/adv_2021/day_04.clj
@@ -6,14 +6,30 @@
 (def rows 5)
 (def cols 5)
 
+(defn new-board
+  ([] (new-board []))
+  ([board] board))
+
+(defn board-add-row
+  [board row]
+  (when (= cols (count row))
+    (apply conj board row)))
+
+(defn row-col->board-pos
+  [[row col]]
+  (+ (* row cols) col))
+
+(defn board-remaining-vals
+  [board]
+  (remove nil? board))
+
+(defn board-remove-num
+  [board n]
+  (mapv #(when (not= n %) %) board))
+
 (defn str->draws
   [s]
-  (map
-   #(Integer/parseInt %)
-   (-> s
-       (str/split #"\n")
-       first
-       (str/split #","))))
+  (map #(Integer/parseInt %) (str/split s #",")))
 
 (defn str->board
   [s]
@@ -21,53 +37,38 @@
        str/split-lines
        (map str/trim)
        (map (fn [s] (map #(Integer/parseInt %) (str/split s #" +"))))
-       (map #(vector %1 (vec %2)) (range rows))
-       (reduce (fn [acc [row xs]]
-                 (apply
-                  merge
-                  acc
-                  (for [col (range cols)]
-                    {[row col] (nth xs col)})))
-               (sorted-map))))
+       (reduce #(board-add-row %1 %2) (new-board))))
 
-(defn row-completed?
-  [row board]
-  (->> board
-       keys
-       (filter (fn [[r _]] (= r row)))
-       count
-       (= 0)))
-
-(defn col-completed?
-  [col board]
-  (->> board
-       keys
-       (filter (fn [[_ c]] (= c col)))
-       count
-       (= 0)))
-
-(defn bingo?
+(defmacro bingo?
+  "Create the tree of conditionals and board accesssors at compile time.
+  (bingo? []) -> (or (not (or (nth [] 0)
+                              (nth [] 1)
+                              (nth [] 2)
+                              (nth [] 3)
+                              (nth [] 4)))
+                     (not (or ...))
+                     (not (or ...))
+                     ...)
+  The individual (not (or ...)) branches are rows and cols over the board.
+  We check if any of them contain a value, if there is some number left in
+  that row/col, it can't be the winning row/col and we check the next one."
   [board]
-  (some identity
-        (concat
-         (for [row (range rows)]
-           (row-completed? row board))
-         (for [col (range cols)]
-           (col-completed? col board)))))
-
-(defn remove-num
-  [n board]
-  (->> board
-       (remove #(= n (val %)))
-       (into {})))
+  `(or ~@(for [cols (vector (vec (range cols)))
+               row (range rows)]
+           `(not (or ~@(for [col cols]
+                         `(nth ~board ~(row-col->board-pos [row col]))))))
+       ~@(for [rows (vector (vec (range rows)))
+               col (range cols)]
+           `(not (or ~@(for [row rows]
+                         `(nth ~board ~(row-col->board-pos [row col]))))))))
 
 (defn play-boards-until-winner
   [draws boards]
   (loop [draws draws
          boards boards]
     (let [cur-draw (first draws)
-          new-boards (map #(remove-num cur-draw %) boards)
-          winning-board (first (seq (filter bingo? new-boards)))]
+          new-boards (map #(board-remove-num % cur-draw) boards)
+          winning-board (first (filter #(bingo? %) new-boards))]
       (if winning-board
         [cur-draw winning-board]
         (recur (rest draws) new-boards)))))
@@ -75,7 +76,7 @@
 (defn calculate-score
   [last-draw winning-board]
   (* last-draw
-     (apply + (vals winning-board))))
+     (apply + (board-remaining-vals winning-board))))
 
 (defn solve-1
   [input]
@@ -91,11 +92,11 @@
   (loop [draws draws
          boards boards]
     (let [cur-draw (first draws)
-          new-boards (map #(remove-num cur-draw %) boards)]
+          new-boards (map #(board-remove-num % cur-draw) boards)]
       (if (and (= 1 (count new-boards))
                (bingo? (first new-boards)))
         [cur-draw (first new-boards)]
-        (recur (rest draws) (remove bingo? new-boards))))))
+        (recur (rest draws) (remove #(bingo? %) new-boards))))))
 
 (defn solve-2
   [input]
