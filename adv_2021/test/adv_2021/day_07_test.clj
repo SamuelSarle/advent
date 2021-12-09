dda2479624dc556173470ab6bbe6ff62fb375172
Samuel Sarle <samuel@sarle.com>
2021-12-09 12:21:49 +0200

    Remove unused require

--- adv_2021/test/adv_2021/day_07_test.clj
+++ adv_2021/test/adv_2021/day_07_test.clj
@@ -1,7 +1,6 @@
 (ns adv-2021.day-07-test
   (:require
    [adv-2021.day-07 :refer [solve-1 solve-2]]
-   [clojure.string :as str]
    [clojure.test :refer :all]))
 
 (def test-input "16,1,2,0,4,2,7,1,2,14")
